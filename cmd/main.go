21f23fcb1de3ceb3f020e3fb68d162562df0da6f
Jason Borneman <jborneman@gmail.com>
2022-11-19 15:45:11 -0500

    Combine local http serve for callbacks into one.

--- cmd/main.go
+++ cmd/main.go
@@ -18,23 +18,45 @@ import (
 	yt "google.golang.org/api/youtube/v3"
 )
 
+const spotifyRedirectURI = "http://localhost:8080/callback"
+const youtubeRedirectURI = "http://localhost:8080/youtube"
+
+var (
+	spotifyAuth = spotifyauth.New(spotifyauth.WithRedirectURL(spotifyRedirectURI), spotifyauth.WithScopes(
+		spotifyauth.ScopeUserReadPrivate,
+		spotifyauth.ScopePlaylistModifyPublic,
+		spotifyauth.ScopeImageUpload))
+	chs   = make(chan *spotifyapi.Client)
+	chg   = make(chan string)
+	state = "abc123"
+)
+
 func main() {
 	scrapeCoSo := os.Getenv("CSM_SCRAPE_COSO") == "true"
 	doToot := os.Getenv("CSM_DO_TOOT") == "true"
 
-	fmt.Println("     ████                              ")
-	fmt.Println("   ██░░░░/█                            ")
-	fmt.Println("  ██░░░░/░░░██                          ")
-	fmt.Println("  ██░░░/░░░░██                          ")
-	fmt.Println("██░#CoSoRadio░██                        ")
-	fmt.Println("██░░░░░░░░░░░░██                        ")
-	fmt.Println("██░░░░░░░░░░░░██                        ")
-	fmt.Println("  ██░░░░░░░░██                          ")
-	fmt.Println("    ████████")
-	fmt.Println()
-	fmt.Println(fmt.Sprintf("----------] Scrape CoSo: %t", scrapeCoSo))
-	fmt.Println(fmt.Sprintf("----------] Do Toot: %t", doToot))
-	fmt.Println("-----------------------------------------")
+	log.Println("     ████                              ")
+	log.Println("   ██░░░░/█                            ")
+	log.Println("  ██░░░░/░░░██                          ")
+	log.Println("  ██░░░/░░░░██                          ")
+	log.Println("██░#CoSoRadio░██                        ")
+	log.Println("██░░░░░░░░░░░░██                        ")
+	log.Println("██░░░░░░░░░░░░██                        ")
+	log.Println("  ██░░░░░░░░██                          ")
+	log.Println("    ████████")
+	log.Println()
+	log.Println(fmt.Sprintf("----------] Scrape CoSo: %t", scrapeCoSo))
+	log.Println(fmt.Sprintf("----------] Do Toot: %t", doToot))
+	log.Println("-----------------------------------------")
+	log.Println("Starting HTTP server for auth callbacks...")
+	http.HandleFunc("/callback", completeSpotifyAuth)
+	http.HandleFunc("/youtube", completeYoutubeAuth)
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	var spotifyClient *spotifyapi.Client
 	spotifyClient = StartSpotifyAuthProcess()
@@ -54,41 +76,19 @@ func main() {
 	}
 }
 
-const spotifyRedirectURI = "http://localhost:8080/callback"
-const youtubeRedirectURI = "http://localhost:8081/youtube"
-
-var (
-	auth = spotifyauth.New(spotifyauth.WithRedirectURL(spotifyRedirectURI), spotifyauth.WithScopes(
-		spotifyauth.ScopeUserReadPrivate,
-		spotifyauth.ScopePlaylistModifyPublic,
-		spotifyauth.ScopeImageUpload))
-	ch    = make(chan *spotifyapi.Client)
-	chg   = make(chan string)
-	state = "abc123"
-)
-
 func StartSpotifyAuthProcess() *spotifyapi.Client {
-	// first start an HTTP server
-	http.HandleFunc("/callback", completeSpotifyAuth)
-	go func() {
-		err := http.ListenAndServe(":8080", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	url := auth.AuthURL(state)
-	fmt.Println("SPOTIFY AUTH")
-	fmt.Println("\tTaking you to Spotify for authentication!")
+	url := spotifyAuth.AuthURL(state)
+	log.Println("SPOTIFY AUTH")
+	log.Println("\tTaking you to Spotify for authentication!")
 	exec.Command("open", url).Start()
 
 	// wait for auth to complete
-	client := <-ch
+	client := <-chs
 	return client
 }
 
 func completeSpotifyAuth(w http.ResponseWriter, r *http.Request) {
-	tok, err := auth.Token(r.Context(), state, r)
+	tok, err := spotifyAuth.Token(r.Context(), state, r)
 	if err != nil {
 		http.Error(w, "\tCouldn't get token", http.StatusForbidden)
 		log.Fatal(err)
@@ -99,8 +99,8 @@ func completeSpotifyAuth(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// use the token to get an authenticated client
-	client := spotifyapi.New(auth.Client(r.Context(), tok))
-	ch <- client
+	client := spotifyapi.New(spotifyAuth.Client(r.Context(), tok))
+	chs <- client
 }
 
 func StartGoogleAuthProcess() (*yt.Service, error) {
@@ -126,9 +126,10 @@ func StartGoogleAuthProcess() (*yt.Service, error) {
 }
 
 func WriteToClientSecretJson() error {
-	googleAuthJson := `{"installed":{"client_id":"THEID","project_id":"cosoradio","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"THESECRET","redirect_uris":["http://localhost:8081/youtube"]}}`
+	googleAuthJson := `{"installed":{"client_id":"THEID","project_id":"cosoradio","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"THESECRET","redirect_uris":["THEREDIRECT"]}}`
 	googleAuthJson = strings.Replace(googleAuthJson, "THEID", os.Getenv("GOOGLE_ID"), 1)
 	googleAuthJson = strings.Replace(googleAuthJson, "THESECRET", os.Getenv("GOOGLE_SECRET"), 1)
+	googleAuthJson = strings.Replace(googleAuthJson, "THEREDIRECT", youtubeRedirectURI, 1)
 	f, err := os.Create("Temp/client_secret.json")
 	defer f.Close()
 	if err != nil {
@@ -159,17 +160,8 @@ func completeYoutubeAuth(w http.ResponseWriter, r *http.Request) {
 // getTokenFromWeb uses Config to request a Token.
 // It returns the retrieved Token.
 func getGoogleTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	// first start an HTTP server
-	http.HandleFunc("/youtube", completeYoutubeAuth)
-	go func() {
-		err := http.ListenAndServe(":8081", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Println("\tTaking you to Google for authentication!")
+	log.Println("\tTaking you to Google for authentication!")
 	exec.Command("open", authURL).Start()
 
 	code := <-chg
